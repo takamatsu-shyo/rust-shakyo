8aac1008a299b36e52f69ffb6a3b43880f742a66
Sho Yano <takamatsu.shyo@gmail.com>
2025-09-19 13:48:40 +0300

    echor

---
+++ echor/src/main.rs
@@ -0,0 +1,29 @@
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new("echor")
+        .version("0.1.0")
+        .author("SY")
+        .about("Rust echo shakyo")
+        .arg(
+            Arg::with_name("text")
+            .value_name("TEXT")
+            .help("Input text")
+            .required(true)
+            .min_values(1),
+            )
+        .arg(
+            Arg::with_name("omit_newline")
+            .short("n")
+            .help("Do not print new line")
+            .takes_value(false),
+            )
+        .get_matches();
+
+    //println!("{:#?}", matches);
+
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
+    
+    print!("{}{}", text.join(" "), if omit_newline {""} else {"\n"});
+}
